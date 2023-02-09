f0b9a9094491ca4b40e17f97a3a01bcf247b7e78
cainnn <cainnn@126.com>
2023-02-10 00:11:47 +0800

    添加项目文件。

---
+++ WebScraping/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using System.Threading.Channels;
+
+namespace WebScraping
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            string html = await new HttpClient().GetStringAsync("http://unify1.gx.cmcc:10020/employeePerf/perfFeedBackAction.do?method=showToRead&activityid=1675769375059&mainAssessId=2946105&showBtn=true&feeback_kind=03%22");
+            Console.WriteLine(html);
+        }
+    }
+}
